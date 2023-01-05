7f9006834abe697ef27fa35840511d378a818414
harshraj2717 <harshraj2717@gmail.com>
2023-01-05 22:11:08 +0530

    completed second minimum

--- Random/second minimum.cpp
+++ Random/second minimum.cpp
@@ -5,20 +5,26 @@
 
 using namespace std;
 
-void minimums(vector<int>& nums, int n){
-    int minn = nums[0], sec_minn = nums[0];
+void minimums(vector<int>& nums){
+    int n = nums.size();
+    if(n == 1){
+        cout << nums[0] << endl;
+        return;
+    }
+    int minn=INT_MAX, sec_minn=INT_MAX;
 
     for(int i=0; i<n; i++){
-        if(nums[i] < minn) minn = nums[i];
+        if(nums[i] < minn){
+            sec_minn = minn;
+            minn = nums[i];
+        }
+        else if(nums[i] < sec_minn && nums[i] != minn){
+            sec_minn = nums[i];
+        }
     }
 
-    if(minn == nums[0]) sec_minn = nums[1];
-
-    for(int i=0; i<n; i++){
-        if(nums[i] != minn && nums[i] < sec_minn) sec_minn = nums[i];
-    }
-
-    cout << minn << " " << sec_minn << endl;
+    if(sec_minn == INT_MAX) cout << minn << " -1" << endl;
+    else cout << minn << " " << sec_minn << endl;
 }
 
 int main(){
@@ -29,7 +35,7 @@ int main(){
         cin >> nums[i];
     }
 
-    minimums(nums, n);
+    minimums(nums);
 
     return 0;
 }
