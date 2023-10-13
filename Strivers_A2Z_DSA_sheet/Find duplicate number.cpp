2a2aea8ebf6b5f5f131fe475db0e1b1af0aa8feb
harshraj2717 <harshraj2717@gmail.com>
2023-10-13 23:09:46 +0530

    update find duplicate number

--- Strivers_A2Z_DSA_sheet/Find duplicate number.cpp
+++ Strivers_A2Z_DSA_sheet/Find duplicate number.cpp
@@ -1,23 +1,56 @@
 // Problem : https://leetcode.com/problems/find-the-duplicate-number/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int findDuplicate(vector<int>& nums) {
-    for(int i=0; i<nums.size(); i++) {
-        int temp = abs(nums[i]) - 1;
-        if(nums[temp] < 0) return abs(nums[i]);
-        nums[temp] *= -1;
+int findDuplicate(vector<int> &nums)
+{
+    // -------------------------------------------
+    // visiting each index of the array and setting it to negative just to know if that index has been visited
+    // since atleast one element will repeat hence atleast one index will be visited twice
+    // -------------------------------------------
+    // for(int i=0; i<nums.size(); i++) {
+    //     int temp = abs(nums[i]) - 1;
+    //     if(nums[temp] < 0) return abs(nums[i]);
+    //     nums[temp] *= -1;
+    // }
+    // return 0;
+
+    // -------------------------------------------
+    // Using bit manipulation
+    // -------------------------------------------
+    int ans = 0;
+    for (int i = 0; i < 21; i++)
+    {
+        int temp = 1 << i;
+        if (temp > nums.size())
+            break;
+
+        int actual = 0;
+        int expected = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            // actual number of 1s
+            if (nums[j] & temp)
+                actual++;
+            // expected number of 1s
+            if (j & temp)
+                expected++;
+        }
+        if (actual > expected)
+            ans += temp;
     }
-    return 0;
+    return ans;
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
-    vector<int> nums(n,0);
-    for(int i = 0; i < n; i++){
+    vector<int> nums(n, 0);
+    for (int i = 0; i < n; i++)
+    {
         cin >> nums[i];
     }
 
